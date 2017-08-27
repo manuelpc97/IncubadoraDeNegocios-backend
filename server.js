5a33129054a16a9d4fa4fde6239158cadce0a427
manuelpc97 <mcanelo41@gmail.com>
2017-08-26 21:14:43 -0600

    eady to Start

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ server.connection({
    });
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/Incubadora')
-//mongoose.connect('mongodb://juany:password@ds155651.mlab.com:55651/laboratorio')
+mongoose.connect('mongodb://127.0.0.1:27017/IncubadoraDeNegocios');
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback() {
